c7d8c578e78de4eb42240c300391d5034b00d518
aryasreerag <76676572+aryasreerag@users.noreply.github.com>
2021-05-11 21:31:07 +0530

    Add files via upload

--- Simple.java
+++ Simple.java
@@ -1,5 +1,15 @@
-class Simple{
-public static void main(String args[]){
+class A{
+private int data=40;
+private void msg()
+{
 System.out.println("Hello java");
+}
+}
+public class Simple{
+ public static void main(String ags[])
+{
+	A obj=new A();
+	System.out.println(obj.data);//Compile Time Error
+	obj.msg();//Compile Time Error
 }
 }
